593bd7b83966a1f898cf51a67c03ed90856183f6
Himanshu Masant <himanshumasant@uxdlabtech.com>
2023-03-25 16:57:09 +0530

    updates

--- src/firebase/cloudFirestore/deleteData.jsx
+++
@@ -1,9 +0,0 @@
-import { doc, deleteDoc } from "firebase/firestore";
-import { db } from "../firebase";
-
-// Delete a Document from a Collection
-export const deleteDocument = async (collectionName, documentName) => {
-  const res = deleteDoc(doc(db, collectionName, documentName));
-
-  return res;
-};
--- src/firebase/firebaseAuth/userLogout.jsx
+++
@@ -1,8 +0,0 @@
-import { auth } from "../firebase";
-
-// Logout users from the website
-export const userLogout = async () => {
-  const res = auth.signOut();
-
-  return res;
-};
